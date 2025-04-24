3201911133f7e20f7f6b5871995855211a6f0217
Daniel-LSP <gcinovation12@gmail.com>
2025-04-23 19:55:50 -0500

    Lewin

---
+++ 23_de_abril/LEWIN/main.py
@@ -0,0 +1,26 @@
+'''
+realizar las importaciones de las funciones 
+segun las 4 variantes vistas hacer print a la ejecucion de las 
+funciones importadas
+'''
+
+from operaciones import sumar 
+print(sumar(4,5))
+
+from operaciones import restar
+print(restar(9,10))
+
+from operaciones import multiplicar
+print(multiplicar(2,2))
+
+from operaciones import dividir
+print(dividir(8,2))
+
+from operaciones import modulo
+print(modulo(9,2))
+
+from operaciones import division_piso
+print(division_piso(9,2))
+
+from operaciones import numero_aleatorio
+print(numero_aleatorio())
